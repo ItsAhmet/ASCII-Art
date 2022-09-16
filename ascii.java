90b5ab776c0889f94eb924922399a963ea36219f
Ahmet Yesilbas <74107960+ItsAhmet@users.noreply.github.com>
2022-09-16 17:19:07 -0400

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,16 @@
+public class ascii {
+  public static void main(String[] args) {
+   System.out.println("                  ___");
+   System.out.println("                / >     >");
+   System.out.println("               |   _  _ ∣");
+   System.out.println("              / ` _x  ⎠");
+   System.out.println("            /         |");
+   System.out.println("            /    ⎩     ⎠");
+   System.out.println("            /   _|  ∣  |");
+   System.out.println("            |_ /_|  ∣  |");
+   System.out.println("              |( `__`_)__)");
+   System.out.println("              \\三>");
+   
+   
+  }
+}
